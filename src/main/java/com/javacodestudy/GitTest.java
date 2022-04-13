5982a8fd94cf19c7d3e4cabe84c1f048ade034d6
xucan <1843010304@qq.com>
2022-04-13 17:11:15 +0800

    first commit

---
+++ src/main/java/com/javacodestudy/GitTest.java
@@ -0,0 +1,15 @@
+package com.javacodestudy;
+
+/**
+ * @description:
+ * @author: Mr.xu
+ * @create: 2022-04-13 17:02
+ **/
+
+public class GitTest {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Git!");
+    }
+}
