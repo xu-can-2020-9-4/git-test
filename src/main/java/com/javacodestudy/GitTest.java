9104d4624a8fbc40d8b5153f22198008a60651f0
许灿 <70761916+xu-can-2020-9-4@users.noreply.github.com>
2022-04-14 11:26:34 +0800

    Update GitTest.java

--- src/main/java/com/javacodestudy/GitTest.java
+++ src/main/java/com/javacodestudy/GitTest.java
@@ -10,15 +10,6 @@ public class GitTest {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello Git!");
-        System.out.println("Hello Git2!");
-        System.out.println("Hello Git3!");
-        System.out.println("Hello Git4!");
-        System.out.println("Hello Git5!");
-        System.out.println("push test!");
-
-        System.out.println("pull test!");
-
-        System.out.println("push test2");
+        System.out.println("Hello World!");
     }
 }
